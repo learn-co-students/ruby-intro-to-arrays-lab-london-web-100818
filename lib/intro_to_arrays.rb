855c9afbf1c7747473743dfd53c96d95a28376e6
Lola Onanuga <slonanuga@gmail.com>
2018-09-24 18:30:37 +0000

    basic arrays

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,34 @@
+def instantiate_new_array
+  my_new_array = []
+  
+end
+
+def array_with_two_elements
+  my_two_array = ["a","b"]
+end
+
+def first_element(my_first_element)
+my_first_element[0]
+end
+
+def third_element(my_third_element)
+my_third_element[2]
+end
+
+def last_element(my_last_element)
+my_last_element[-1]
+end
+
+def first_element_with_array_methods(first_element_array)
+first_element_array.first
+end
+
+def last_element_with_array_methods(last_element_array)
+last_element_array.last
+end
+
+def length_of_array(my_length_of_array)
+  my_length_of_array.size
+end
+
+
